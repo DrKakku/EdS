ddc4476409de61d1e479c4cc12f72e4a87727f70
arreyhul <66854826+arreyhul@users.noreply.github.com>
2020-10-26 23:09:45 +0530

    added Theme to Attendance

--- src/Attendance/AttendanceComponent.js
+++ src/Attendance/AttendanceComponent.js
@@ -2,22 +2,23 @@ import { StatusBa } from "expo-status-bar";
 import React, { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import Subjects from "./Subjects";
+import styles from "./Theme";
 
 export default function Attendance() {
   return (
-    <View style={styles.content}>
+    <View style={styles.viewProperties}>
       <Subjects />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
+/*const styles = StyleSheet.create({
   BG: {
     backgroundColor: "#fff",
     flex: 1,
   },
   content: {
     flex: 1,
-    padding: 20,
+    marginTop: 50,
   },
-});
+});*/
---
+++ src/Attendance/Theme.js
@@ -0,0 +1,11 @@
+import { StyleSheet } from "react-native";
+const Theme = StyleSheet.create({
+  viewProperties: {
+    marginTop: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    flex: 1,
+  },
+});
+
+export default Theme;
