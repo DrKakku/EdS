d5da0774ff6bcf44d47e4e439b8665902bb71016
Yash Tripathi <53153448+DrKakku@users.noreply.github.com>
2020-10-27 16:08:46 +0530

    removed the baground change capability of the app
    The touchable from the title of the app was removed and the styles and the background elements were removed from the code.

--- src/Task/TaskComponent.js
+++ src/Task/TaskComponent.js
@@ -14,14 +14,12 @@ import Elevation from "react-native-elevation";
 import InputTask from "./textInp";
 import GoalItem from "./goalItem";
 
-const bg2 = require("../../assets/baground2.jpg");
+
 
 export default function Start() {
   const [TaskState, setTaskState] = useState(false);
   const [TaskState1, setTaskState1] = useState(false);
-  const [bg1Visiable, setBg1Visiablity] = useState(false);
-  const [bg2Visiable, setBg2Visiablity] = useState(false);
-  const [BG, setBG] = useState(require("../../assets/baground1.jpg"));
+  const [BG, setBG] = useState();
   const taskStateHandler = () => {
     setTaskState(true);
   };
@@ -49,106 +47,11 @@ export default function Start() {
     setTaskState(false);
   };
 
-  const screenHandler = () => {
-    if (bg1Visiable) {
-      setBg1Visiablity(false);
-      setBg2Visiablity(true);
-      setBG(require("../../assets/baground2.jpg"));
-    } else if (bg2Visiable) {
-      setBg1Visiablity(false);
-      setBg2Visiablity(false);
-      setBG(require("../../assets/baground1.jpg"));
-    } else {
-      setBg1Visiablity(true);
-      setBg2Visiablity(false);
-      setBG(require("../../assets/baground1.jpg"));
-    }
-  };
+  
 
   const bgStyle = () => {
-    if (bg1Visiable) {
-      const styleBG1 = StyleSheet.create({
-        screen: {
-          height: "100%",
-          width: "100%",
-          backgroundColor: "rgba(153, 123, 102,0.3)",
-        },
-
-        body: {
-          height: "99%",
-          justifyContent: "center",
-          margin: "1%",
-          marginBottom: "3%",
-          backgroundColor: "rgba(157, 107, 83,-0.2)",
-          flexDirection: "column",
-          alignItems: "center",
-          borderRadius: 15,
-        },
-        textcol: {
-          fontSize: 50,
-          color: "#463f3a",
-        },
-        themeCol: {
-          color: "#606c38",
-          fontSize: 45,
-          fontWeight: "bold",
-        },
-        task: {
-          justifyContent: "center",
-          alignItems: "center",
-          minWidth: "70%",
-          padding: 10,
-          margin: 4,
-          borderBottomColor: "grey",
-          borderRadius: 30,
-          backgroundColor: "#f2effe",
-          ...Elevation[4],
-        },
-      });
-
-      return styleBG1;
-    } else if (bg2Visiable) {
-      const styleBG2 = StyleSheet.create({
-        screen: {
-          height: "100%",
-          width: "100%",
-          backgroundColor: "rgba(255,255,255,.1)",
-        },
-
-        body: {
-          height: "99%",
-          justifyContent: "center",
-          margin: "1%",
-          marginBottom: "3%",
-          backgroundColor: "rgba(100,100,100,.2)",
-          flexDirection: "column",
-          alignItems: "center",
-          borderRadius: 30,
-        },
-        textcol: {
-          fontSize: 50,
-          color: "#1a535c",
-        },
-        themeCol: {
-          color: "#73d2de",
-          fontSize: 48,
-          fontWeight: "bold",
-        },
-        task: {
-          justifyContent: "center",
-          alignItems: "center",
-          minWidth: "90%",
-          padding: 10,
-          margin: 4,
-          borderBottomColor: "grey",
-          borderRadius: 30,
-          backgroundColor: "#f2effe",
-          ...Elevation[3],
-        },
-      });
-
-      return styleBG2;
-    } else {
+   
+    
       const styleNBG = StyleSheet.create({
         screen: {
           height: "100%",
@@ -190,19 +93,16 @@ export default function Start() {
         },
       });
       return styleNBG;
-    }
+    
   };
 
   return (
-    <ImageBackground
-      source={BG}
-      style={{ resizeMode: "cover", flex: 1, justifyContent: "center" }}
-    >
+   
       <View style={bgStyle().screen}>
         {/* // Body */}
         <View style={bgStyle().body}>
           <View style={styles.header}>
-            <TouchableHighlight onPress={screenHandler}>
+            
               {/* //text */}
               <View style={{ ...Elevation[1] }}>
                 <Text style={bgStyle().textcol}>
@@ -210,7 +110,7 @@ export default function Start() {
                   S<Text style={bgStyle().themeCol}>uch</Text>i
                 </Text>
               </View>
-            </TouchableHighlight>
+            
 
             {/* //input text and button container */}
             <View>
@@ -253,7 +153,7 @@ export default function Start() {
           </View>
         </View>
       </View>
-    </ImageBackground>
+    
   );
 }
 
